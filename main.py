a7d59f1ab018e52b5acb10748da9651628dc5ab2
Alan Benavides <bruno@coastt.org>
2022-07-17 21:38:37 -0400

    add: create person

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_maried: Optional[bool] = None
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-    
+
+@app.post('/person')
+def create_person(person: Person = Body(...)):
+    return person.dict()
