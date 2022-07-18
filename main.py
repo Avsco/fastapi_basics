ce2b9f6f21c1b8ae8d497c9f0242a69fe55c1250
Alan Benavides <bruno@coastt.org>
2022-07-17 21:55:27 -0400

    add: query params

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Path, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -18,3 +18,20 @@ def root():
 @app.post('/person')
 def create_person(person: Person = Body(...)):
     return person.dict()
+
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: str = Query(
+        ...,
+        title="Person age",
+        description="This is the person age. It's required"
+        )
+):
+    return {name: age}
